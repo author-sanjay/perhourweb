e6c1e6ecbfca2bc0320e277a6ba425b13268af63
sam <sameerda3@gmail.com>
2023-03-03 12:47:11 +0530

    profilepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import Profilepage from "./Components/Profile/ProfilePage";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
               className="flex flex-row"
               style={{ backgroundColor: "#fff", minHeight: "100vh" }}
             >
-              <Home />
+              {/* <Home /> */}
+              <Profilepage/>
             </div>
           }
         />
